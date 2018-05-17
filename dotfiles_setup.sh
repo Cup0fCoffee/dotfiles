65933ea14ce4b148e65686dec9e1647726542b6b
Maxim Beder <macsim.beder@gmail.com>
2018-05-17 11:42:00 +0200

    Added tmux config files. Created and configured '.tmuxline.conf' for style. Added '.tmuxline.conf' to 'dotfiles_setup.sh'.

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dotfiles=(".bash_profile" ".fzf.bash" ".git-completion.bash" ".git-prompt.sh" 
-          ".gitconfig" ".vimrc" ".tmux.conf")
+          ".gitconfig" ".vimrc" ".tmux.conf" ".tmuxline.conf")
 dir="${HOME}/.dotfiles"
 
 for dotfile in "${dotfiles[@]}"; do
