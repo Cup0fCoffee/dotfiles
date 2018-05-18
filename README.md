5acfdc79cbd0498cbb2f7021400749a01b90d810
Maxim Beder <macsim.beder@gmail.com>
2018-05-18 21:30:57 +0200

    Rewrote the setup script to be more verbose and (hopefully) less error prone. Added colored output. Moved completed todoes to the bottom of the list.

--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 My dotfiles.
 
 # To Do:
-- [x] Add to do
-- [x] Recover plugins from the plugins tracklist
-- [x] Finish adding standart settings
 - [ ] Add descriptions to the top of each file
-- [x] Set up tmux configs (add nice window management shortcuts)
-- [x] Move plugin tracker file to dotfiles??? (optional)
+    - _Or write a description to each of the files in the README.md_
 - [ ] Write a description of how is this maintained and used
-- [x] ~~Symlinks vs sourcing files?~~ (symlinks for now)
 - [ ] Create a list of dependencies and app used
 - [ ] Write a script that would install all require dependecies
 - [ ] Write a script for scheduling sleep and wake (use `pmset schedule`)
-- [ ] Rewrite the setup script to be less error prone and to add clean up?
+- [x] Rewrite the setup script to be less error prone and verbose
+    - [ ] Add an option to move the confilicting files in the home dir to
+        another one.
+- [x] Add to do
+- [x] Recover plugins from the plugins tracklist
+- [x] Finish adding standart settings
+- [x] Set up tmux configs (add nice window management shortcuts)
+- [x] Move plugin tracker file to dotfiles??? (optional)
+- [x] ~~Symlinks vs sourcing files?~~ (symlinks for now)
 - [x] Ensure mouse support in both tmux and vim
 - [x] Remove the dots ('.') from file names and make the setup script add them 
--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,9 +1,80 @@
 #!/bin/bash
 
-dotfiles=("bash_profile" "fzfbash" "git-completionbash" "git-promptsh" 
-          "gitconfig" "vimrc" "tmuxconf" "tmuxlineconf")
-dir="${HOME}/.dotfiles"
+# Colors
+NC='\033[0m' # No color
+LT_GREEN='\033[1;32m' # Light Green
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+LT_CYAN='\033[1;36m' # Light Cyan
+YELLOW='\033[0;33m'
+LT_BLUE='\033[1;34m'
+
+dotfiles=("bash_profile" "fzf.bash" "git-completion.bash" "git-prompt.sh"
+          "gitconfig" "vimrc" "tmux.conf" "tmuxline.conf")
+dir="$(pwd)"
+
+echo -e "Going to create links for this files in the home directory: "
+for dotfile in "${dotfiles[@]}"; do
+    echo -e "${LT_CYAN}${dotfile}${NC}"
+done
+echo -e
+
+echo -en "Proceed? (${GREEN}y${NC}/n) "
+read ans
+
+if [ $ans != "y" ]; then
+    echo "Exiting..."
+    exit 1
+fi
+
+if [ $PLATFORM = "Darwin" ] && [ -x "/usr/local/bin/gln" ]; then
+    ln_command=gln
+else
+    ln_command=ln
+fi
+
+echo "bash command used to create links: ${ln_command}"
+
+if [ $PLATFORM = "Darwin" ] && [ -x "/usr/local/bin/greadlink" ]; then
+    readlink_command="greadlink -f"
+else
+    readlink_command="readlink"
+fi
+
+echo "bash command used to read links: ${readlink_command}"
+
+failed=()
 
 for dotfile in "${dotfiles[@]}"; do
-	gln -sf "${dir}/${dotfile}" "${HOME}/.${dotfile}" 
+    origin="${dir}/${dotfile}"
+    destination="${HOME}/.${dotfile}"
+    echo -e "Creating a soft link for ${YELLOW}${origin}${NC}" \
+         "in ${LT_BLUE}${destination}...${NC}"
+    if [ -h ${destination} ]; then
+        if [ $($readlink_command ${destination}) = ${origin} ]; then
+            echo -e "${LT_GREEN}Already exists.${NC}"
+        else
+            echo -e "${RED}A link exists, but points to another file:" \
+                    "$($readlink_command ${destination})${NC}"
+            failed+=("${destination}")
+        fi
+    elif [ -f ${destination} ]; then
+        echo -e "${RED}A file exists. Delete it (or temporary move it).${NC}"
+        failed+=("${destination}")
+    else
+        $ln_command -s "${origin}" "${destination}"
+        echo -e "${GREEN}Created.${NC}"
+    fi
 done
+
+clear;
+
+if [ ${#failed[@]} -gt 0 ]; then
+    echo -e "${RED}Failed to create links for: "
+    for fail in ${failed[@]}; do
+        echo -e "${fail}"
+    done
+    echo -e "${NC}"
+else
+    echo -e "${GREEN}Done.${NC}"
+fi
